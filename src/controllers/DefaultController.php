511a15afb1df3563bb709b791511d330b7d36ab1
Aphichat Panjamanee <panjamanee@gmail.com>
2020-04-16 20:39:30 +0200

    Changed authenticate method to a simple query string, because some hosts dont allow PHP_AUTH_USER

--- src/controllers/DefaultController.php
+++ src/controllers/DefaultController.php
@@ -69,23 +69,9 @@ class DefaultController extends Controller
         header('Cache-Control: no-cache, must-revalidate, max-age=0');
 
         $token = \daystatus\daystatus\Daystatus::getInstance()->getSettings()->someAttribute;
-        $set = explode(':', $token);
-
-        $AUTH_USER = $set[0];
-        $AUTH_PASS = $set[1];
-
-        $has_supplied_credentials = !(empty($_SERVER['PHP_AUTH_USER']) && empty($_SERVER['PHP_AUTH_PW']));
-
-        $is_not_authenticated = (
-            !$has_supplied_credentials ||
-            $_SERVER['PHP_AUTH_USER'] != $AUTH_USER ||
-            $_SERVER['PHP_AUTH_PW']   != $AUTH_PASS
-        );
-
-        if ($is_not_authenticated)
+        
+        if ($_SERVER['QUERY_STRING'] !== $token)
         {
-            header('HTTP/1.1 401 Authorization Required');
-            header('WWW-Authenticate: Basic realm="Access denied"');
             exit;
         }
 
